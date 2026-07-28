87c89de07875003a9919cdaf132abe0511cbf9f4
Dorian Ignee <dorian.ignee@posterselect.de>
2026-07-28 13:46:20 +0200

    feat: add Github-Link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,12 @@
 <h1>Digi-Comp I</h1>
 
 <p>The Digi-Comp I was a plastic toy computer that can be used to demonstrate basic digital logic concepts. 
+  It was marketed as the "first real operating computer in plastic".
   It was invented in 1963 by the company E.S.R. and paved the way for many people who became interested in computer science. 
   Many of these individuals went on to become software developers or engineers.</p>
 
+<p>You can download the instruction manual on <a href="https://archive.org/details/digicomp_i/digi-comp1-manual-c-s/">archive.org</a>.</p>
+
 <DigiComp />
+
+<p>Built 2026 by Dorian Ignee. Feel free to contribute on <a href="https://github.com/dorianignee/Digi-Comp">GitHub</a>.</p>
