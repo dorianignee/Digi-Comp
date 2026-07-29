17e401c79c2460786e8e3bb3bfb517b0792d7315
Dorian Ignee <dorian.ignee@posterselect.de>
2026-07-29 12:06:38 +0200

    chore: move global css to layout.svelte

--- src/lib/components/DigiComp.svelte
+++ src/lib/components/DigiComp.svelte
@@ -292,26 +292,7 @@
     text-align: center;
   }
 
-  :global(html,
-  body) {
-    width: 100%;
-    height: 100%;
-    min-height: 100%;
-  }
-  :global(.flex-row) {
-    display: flex;
-    flex-direction: row;
-    width: fit-content;
-    align-items: stretch;
-  }
 
-  :global(.gap-1) {
-    gap: 1rem;
-  }
-
-  :global(.gap-05) {
-    gap: 0.5rem;
-  }
 
   .flex-col {
     display: flex;
@@ -324,10 +305,6 @@
 
   .align-items-end {
     align-items: flex-end;
-  }
-
-  :global(.flex-item) {
-    flex: 1;
   }
 
   .front-panel {
@@ -348,19 +325,5 @@
   .flip-flops {
     margin-top: 1rem;
     justify-self: flex-end;
-  }
-
-  :global(.tabler--trash) {
-    display: inline-block;
-    width: 1em;
-    height: 1em;
-    --svg: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath fill='none' stroke='black' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M4 7h16m-10 4v6m4-6v6M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2l1-12M9 7V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v3'/%3E%3C/svg%3E");
-    background-color: currentColor;
-    -webkit-mask-image: var(--svg);
-    mask-image: var(--svg);
-    -webkit-mask-repeat: no-repeat;
-    mask-repeat: no-repeat;
-    -webkit-mask-size: 100% 100%;
-    mask-size: 100% 100%;
   }
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,3 +9,45 @@
 </svelte:head>
 
 {@render children()}
+
+<style>
+	:global(body) {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+		margin: 0;
+	}
+	
+	:global(.flex-row) {
+    display: flex;
+    flex-direction: row;
+    width: fit-content;
+    align-items: stretch;
+  }
+
+  :global(.gap-1) {
+    gap: 1rem;
+  }
+
+  :global(.gap-05) {
+    gap: 0.5rem;
+  }
+
+	:global(.flex-item) {
+    flex: 1;
+  }
+
+	:global(.tabler--trash) {
+    display: inline-block;
+    width: 1em;
+    height: 1em;
+    --svg: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath fill='none' stroke='black' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M4 7h16m-10 4v6m4-6v6M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2l1-12M9 7V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v3'/%3E%3C/svg%3E");
+    background-color: currentColor;
+    -webkit-mask-image: var(--svg);
+    mask-image: var(--svg);
+    -webkit-mask-repeat: no-repeat;
+    mask-repeat: no-repeat;
+    -webkit-mask-size: 100% 100%;
+    mask-size: 100% 100%;
+  }
+</style>
